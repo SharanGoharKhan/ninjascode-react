20077dbbdf2649cc91774a87544fc2ab90df4161
Sharan Khan <sharan.gohar@gmail.com>
2019-02-26 16:02:12 +0500

    added boilerplate

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h3 className="checking">Hello From Home</h3>
+            </div>
+        )
+    }
+}
+
+export default Home;
