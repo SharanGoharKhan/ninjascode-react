457c807d29f53099281e6b150b28f2be16b77726
Sharan Khan <sharan.gohar@gmail.com>
2019-02-27 14:40:48 +0500

    completed homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,8 +6,20 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="home-container">
                 <CircleList />
+                <div className="logo-container">
+                    <span>ninja's code</span>
+                </div>
+                <div className="home-text-container">
+                    <div className="home-text-1">
+                        <h3>fast, agile & bootrsapped</h3>
+                    </div>
+                    <div className="home-text-2">
+                        <h3>team of react, react native<br></br>
+                        and node.js developers</h3>
+                    </div>
+                </div>
             </div>
         )
     }
